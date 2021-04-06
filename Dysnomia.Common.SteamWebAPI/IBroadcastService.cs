fb823fd99e6f70e194d45baab88df966c5f4e97f
Elanis <elanis@hotmail.com>
2021-04-06 13:10:14 +0200

    Added IBroadcastService implementation

---
+++ Dysnomia.Common.SteamWebAPI/IBroadcastService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Dysnomia.Common.SteamWebAPI {
+	/// <summary>
+	/// Provides access to Steam broadcasts.
+	/// https://partner.steamgames.com/doc/webapi/IBroadcastService
+	/// </summary>
+	public interface IBroadcastService {
+		/// <summary>
+		/// Add a game meta data frame to broadcast
+		/// </summary>
+		/// <param name="key">Steamworks Web API publisher authentication key.</param>
+		/// <param name="appid"></param>
+		/// <param name="steamid"></param>
+		/// <param name="broadcast_id"></param>
+		/// <param name="frame_data"></param>
+		/// <returns></returns>
+		Task<string> PostGameDataFrame(string key, uint appid, ulong steamid, ulong broadcast_id, string frame_data);
+	}
+}
