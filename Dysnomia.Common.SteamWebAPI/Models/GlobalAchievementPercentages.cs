91324a18f4db35c819ffb1f5d60afb7b01c1266e
Elanis <elanis@hotmail.com>
2025-02-09 10:39:06 +0100

    Fix GlobalAchievementPercentages json deserialization

--- Dysnomia.Common.SteamWebAPI/Models/GlobalAchievementPercentages.cs
+++ Dysnomia.Common.SteamWebAPI/Models/GlobalAchievementPercentages.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using Dysnomia.Common.WebAPIWrapper.Helpers;
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Dysnomia.Common.SteamWebAPI.Models {
-	public class GlobalAchievementPercentagesRoot {
-		public GlobalAchievementPercentagesList achievementpercentages { get; set; }
-	}
+    public class GlobalAchievementPercentagesRoot {
+        public GlobalAchievementPercentagesList achievementpercentages { get; set; }
+    }
 
-	public class GlobalAchievementPercentagesList {
-		public IList<GlobalAchievementPercentages> achievements { get; set; }
-	}
+    public class GlobalAchievementPercentagesList {
+        public IList<GlobalAchievementPercentages> achievements { get; set; }
+    }
 
-	public class GlobalAchievementPercentages {
-		public string name { get; set; }
-		public float percent { get; set; }
-	}
+    public class GlobalAchievementPercentages {
+        public string name { get; set; }
+        [JsonConverter(typeof(StringToFloatConverter))]
+        public float percent { get; set; }
+    }
 }
