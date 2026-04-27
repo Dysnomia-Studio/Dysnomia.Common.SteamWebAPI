f9ecec195374fdb4ed4ff44deb28eed364b3b688
Elanis <elanis@hotmail.com>
2026-04-27 19:28:57 +0200

    GetNumberOfCurrentPlayers() make result nullable for appids without CCU such as hardware

--- Dysnomia.Common.SteamWebAPI/Models/NumberOfCurrentPlayers.cs
+++ Dysnomia.Common.SteamWebAPI/Models/NumberOfCurrentPlayers.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dysnomia.Common.SteamWebAPI.Models {
-	public class NumberOfCurrentPlayers {
-		public ulong player_count { get; set; }
-		public ulong result { get; set; }
-	}
+    public class NumberOfCurrentPlayers {
+        public ulong? player_count { get; set; }
+        public ulong result { get; set; }
+    }
 }
