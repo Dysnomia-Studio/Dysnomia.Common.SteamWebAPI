4652249913a90acff3d50c5f7bd49adb8ef23ca5
Elanis <elanis@hotmail.com>
2026-05-07 11:33:52 +0200

    ISteamStore: Improve Unit tests

--- Dysnomia.Common.SteamWebAPI.Test/SteamStoreTest.cs
+++ Dysnomia.Common.SteamWebAPI.Test/SteamStoreTest.cs
@@ -13,77 +13,109 @@ namespace Dysnomia.Common.SteamWebAPI.Test {
 
         [Fact]
         public async Task GetAppDetails_CS() {
-            await steamStore.GetAppDetails("10");
+            var res = await steamStore.GetAppDetails("10");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_HL() {
-            await steamStore.GetAppDetails("70");
+            var res = await steamStore.GetAppDetails("70");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_HL2() {
-            await steamStore.GetAppDetails("220");
+            var res = await steamStore.GetAppDetails("220");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_TF2() {
-            await steamStore.GetAppDetails("440");
+            var res = await steamStore.GetAppDetails("440");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_CSGO() {
-            await steamStore.GetAppDetails("730");
+            var res = await steamStore.GetAppDetails("730");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_Blender() {
-            await steamStore.GetAppDetails("365670");
+            var res = await steamStore.GetAppDetails("365670");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_Cities_Skyline() {
-            await steamStore.GetAppDetails("255710");
+            var res = await steamStore.GetAppDetails("255710");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_SkyrimSE() {
-            await steamStore.GetAppDetails("489830");
+            var res = await steamStore.GetAppDetails("489830");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_HLAlyx() {
-            await steamStore.GetAppDetails("546560");
+            var res = await steamStore.GetAppDetails("546560");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_Cities_Skyline_SH() {
-            await steamStore.GetAppDetails("1146930");
+            var res = await steamStore.GetAppDetails("1146930");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_Extortion() {
-            await steamStore.GetAppDetails("1299430");
+            var res = await steamStore.GetAppDetails("1299430");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
+            Assert.Null(res.data.fullgame?.appid);
         }
 
         [Fact]
         public async Task GetAppDetails_Extortion_Demo() {
-            await steamStore.GetAppDetails("1355540");
+            var res = await steamStore.GetAppDetails("1355540");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
+            Assert.NotNull(res.data.fullgame?.appid);
         }
 
         [Fact]
         public async Task GetAppDetails_EYE() { // This one has a weird display_type, we add this test to prevent non-compatibility of future modifications
-            await steamStore.GetAppDetails("314260");
+            var res = await steamStore.GetAppDetails("314260");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_XSS() { // This one has some weird data as well
-            await steamStore.GetAppDetails("331840");
+            var res = await steamStore.GetAppDetails("331840");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
 
         [Fact]
         public async Task GetAppDetails_InvalidPercentSaving() {
-            await steamStore.GetAppDetails("1451150");
+            var res = await steamStore.GetAppDetails("1451150");
+            Assert.True(res.success);
+            Assert.NotNull(res.data);
         }
     }
 }
